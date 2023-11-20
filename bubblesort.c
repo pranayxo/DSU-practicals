c84029e8221505116a6a928b9f499b874e367cdc
pranayxo <95767698+pranayxo@users.noreply.github.com>
2023-11-20 23:11:55 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,30 +1,35 @@
 #include<stdio.h>
 #include<conio.h>
-void bubble(int a[], int n);
-int main(){
-  int a[50],n,i;
-  printf("enter the numbeer of elements you want to enter:\n");
-  scanf("%d", &n);
-  printf("Enter the elements:\n");
-  for(i=0;i<;i++){
-    scanf("%d", &a[i]);
-      }
-      bubble(a,n);
-      printf("Sorted array is\n");
-for(i=0;i<n;i++){
-  scanf("%d", &a[i]);
+void bubble(int a[],int n);
+void main()
+{
+int a[50],n,i;
+clrscr();
+printf("Pranay Gaikwad\n");
+printf("enter size of array \n");
+scanf("%d",&n);
+printf("enter array element \n");
+for(i=0;i<=n;i++)
+scanf("%d",&a[i]);
+bubble(a,n);
+getch();
 }
-      return 0;
+void bubble(int a[],int n)
+{
+int i,j,temp;
+for(i=1;i<n;i++)
+{
+ for(j=0;j<n-i;j++)
+ {
+ if(a[j]>a[j+1])
+ {
+ temp=a[j];
+ a[j]=a[j+1];
+ a[j+1]=temp;
+ }
+ }
 }
-void bubble(int a[], int n){
-  int i,j,temp;
-  for(i=1;i<n;i++){
-    for(j=0;j<n-i-1;j++){
-      if(a[j]>a[j+i]){
-      temp=a[j];
-      a[j]=a[j+1];
-      a[j+1]=temp;
-    }
-  }
-  }
+printf("sorted elements are \n");
+for(i=0;i<n;i++)
+printf("%d\t",a[i]);
 }
